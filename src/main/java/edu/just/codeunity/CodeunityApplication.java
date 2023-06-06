65d57a81ad2c42c076df82e3a925fe840f387266
Mohammad Srahin <mohammedasreheen@gmail.com>
2023-06-06 21:06:46 +0300

    Initial commit

---
+++ src/main/java/edu/just/codeunity/CodeunityApplication.java
@@ -0,0 +1,13 @@
+package edu.just.codeunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeunityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodeunityApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/just/codeunity/CodeunityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.just.codeunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeunityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
