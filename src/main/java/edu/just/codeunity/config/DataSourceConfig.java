40a9f9646c90e63ae3670804ef13981e4ec0d11a
iyaad alazzam <40213379+Debo15@users.noreply.github.com>
2023-06-06 22:26:44 +0300

    add dataSource Configuration, enable H2 database

---
+++ src/main/java/edu/just/codeunity/config/DataSourceConfig.java
@@ -0,0 +1,20 @@
+package edu.just.codeunity.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:CodeUnity");
+        dataSourceBuilder.username("admin");
+        dataSourceBuilder.password("123");
+        return dataSourceBuilder.build();
+    }
+}
